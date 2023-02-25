e1a39d18f54da3b4ee87b7150c17b48b859c04ba
hongquylnguyen <hongquy1999@gmail.com>
2023-02-25 15:48:41 -0600

    powerups

--- Assets/Scripts/Powerups/DropUpgrades.cs
+++ Assets/Scripts/Powerups/DropUpgrades.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class DropUpgrades : MonoBehaviour
 {
-    public Powerup upgrade;
+    public Powerup[] upgrade;
+    public int size;
     public float dropChance; // Between 0 and 1
 
     // Start is called before the first frame update
     void Start()
     {
-
     }
 
     // Update is called once per frame
@@ -21,11 +21,13 @@ public class DropUpgrades : MonoBehaviour
 
     public void Drop()
     {
+        int rngDrop = Random.Range(0, size);
+
         float rngNum = Random.Range(0f, 1f);
 
         if (rngNum <= dropChance)
         {
-            Powerup instantiatedUpgrade = Instantiate(upgrade, transform.position, transform.rotation, null);
+            Powerup instantiatedUpgrade = Instantiate(upgrade[rngDrop], transform.position, transform.rotation, null);
         }
     }
 }
