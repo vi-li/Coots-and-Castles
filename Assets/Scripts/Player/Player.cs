cb46c7b7cdcfdce421d5a9164df318e3830c1cfa
Vivian Li <26969962+vi-li@users.noreply.github.com>
2023-03-30 20:43:28 -0700

    Merged release branch and fixed sprites

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,8 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AddressableAssets;
-using UnityEngine.ResourceManagement.AsyncOperations;
 using UnityEngine.Tilemaps;
 
 public class Player : MonoBehaviour
